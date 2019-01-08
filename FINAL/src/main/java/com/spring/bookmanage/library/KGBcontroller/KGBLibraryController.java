a17ffedef806420df1f29941e25702be13d610e7
user1 <user1@DESKTOP-SI9RJ74>
2019-01-08 19:45:35 +0900

    패키지 이름 변경

--- FINAL/src/main/java/com/spring/bookmanage/library/KGBcontroller/KGBLibraryController.java
+++ FINAL/src/main/java/com/spring/bookmanage/library/KGBcontroller/KGBLibraryController.java
@@ -1,4 +1,4 @@
-package com.spring.bookmanage.library.controller;
+package com.spring.bookmanage.library.KGBcontroller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
