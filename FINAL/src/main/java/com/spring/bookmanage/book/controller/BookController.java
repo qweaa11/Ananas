08b08b65d9e82a94e5190226411593c9f2f57aae
user1 <user1@kimkh>
2019-01-08 17:27:19 +0900

    ㅋㅋ

--- FINAL/src/main/java/com/spring/bookmanage/book/controller/BookController.java
+++ FINAL/src/main/java/com/spring/bookmanage/book/controller/BookController.java
@@ -1,17 +1,5 @@
 package com.spring.bookmanage.book.controller;
 
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+public class BookController2 {
 
-@Controller
-@Component
-public class BookController {
-	
-	@RequestMapping(value="/bookList.ana",method= {RequestMethod.GET})
-	public String bookList() {
-		
-		return "bookList.tiles1";
-	}
 }
--- FINAL/src/main/java/com/spring/bookmanage/book/controller/BookController2.java
+++
@@ -1,5 +0,0 @@
-package com.spring.bookmanage.book.controller;
-
-public class BookController2 {
-
-}
