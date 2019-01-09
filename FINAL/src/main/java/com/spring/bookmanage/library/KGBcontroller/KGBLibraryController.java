4ef75ca7ff3d4905b2a0da5513385de216ab179f
user <user@DESKTOP-CCBMHCS>
2019-01-09 18:58:55 +0900

    민후

--- FINAL/src/main/java/com/spring/bookmanage/library/KGBcontroller/KGBLibraryController.java
+++ FINAL/src/main/java/com/spring/bookmanage/library/KGBcontroller/KGBLibraryController.java
@@ -9,14 +9,10 @@ public class KGBLibraryController {
 
 	@RequestMapping(value="/index.ana", method= {RequestMethod.GET})
 	public String index() {
-<<<<<<< HEAD
 
-		return "library/index.tiles1";
-=======
 		
 		return "library/index.tiles1";
 		
->>>>>>> branch 'master' of https://github.com/qweaa11/Ananas.git
 	}// end of index()---------------------
 
 }
